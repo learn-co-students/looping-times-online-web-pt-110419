77056e7f4f516c743a71450df45e4702bdc0baaa
Mintra Phophaijit <bluembc@gmail.com>
2019-11-11 00:40:09 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,25 @@
 def using_times
-	#your code here
+	7.times do
+	  puts "I am doing the dishes left by former friends"
 end
 
+Jewels_in_bag = 100
 
+3. times do
+  puts "Hiding 10 stolen jewels."
+  Jewels_in_bag = Jewels_in_bag - 10
+end
+
+puts "we have #{Jewels_in_bag} jewels still to hide!"
+
+jewels_in_bag = 100
+
+3. times do
+  puts "Hiding 10 stolen jewels." 
+  Jewels_in_bag = Jewels_in_bag - 10
+  puts "Now there are only #{jewels_in_bag} jewels left to hide!"
+end
+
+put "We have #{jewels_in_bag} jewels still to hide!"
 
 
